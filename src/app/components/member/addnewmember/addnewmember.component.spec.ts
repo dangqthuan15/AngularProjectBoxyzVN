d636bad2c9d2b6d1599d815c9699c665509722d2
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-05-16 16:36:54 +0700

    [add] CRUD for user

--- src/app/components/member/addnewmember/addnewmember.component.spec.ts
+++ src/app/components/member/addnewmember/addnewmember.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsersComponent } from './users.component';
+import { AddnewmemberComponent } from './addnewmember.component';
 
-describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+describe('AddnewmemberComponent', () => {
+  let component: AddnewmemberComponent;
+  let fixture: ComponentFixture<AddnewmemberComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [ AddnewmemberComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UsersComponent);
+    fixture = TestBed.createComponent(AddnewmemberComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/components/member/editmember/editmember.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, RequiredValidator, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { query } from '@angular/animations';
+import { UserService } from 'src/app/user.service';
+
+@Component({
+  selector: 'app-editmember',
+  templateUrl: './editmember.component.html',
+  styleUrls: ['./editmember.component.scss']
+})
+export class EditmemberComponent implements OnInit {
+
+  submited: boolean = false;
+  id:any;
+
+  userForm = this.formBuilder.group({
+    full_name:['',Validators.required],
+    username:['',Validators.required],
+    password:['',Validators.required],
+    email:['',Validators.required],
+    DOB : ['',Validators.required]
+  });
+
+  constructor(
+    private userSrv:UserService,
+    private formBuilder:FormBuilder, 
+    public router:Router,
+    private actRouter:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.actRouter.paramMap.subscribe(query=>{
+      this.id= query.get("id");
+      this.userSrv.getByID(this.id).subscribe(res => {
+      let userProfile = res;      
+      this.userForm = this.formBuilder.group({
+        full_name:[userProfile.full_name,Validators.required],
+        username:[userProfile.username,Validators.required],
+        password:[userProfile.password,Validators.required],
+        email:[userProfile.email,Validators.required],
+        DOB : [userProfile.DOB,Validators.required]
+      });
+      })
+    })
+  }
+
+  get f(){return this.userForm.controls;}
+  onSubmit():any{
+    this.submited = true;
+
+    if(this.userForm.invalid){return false;}
+
+    this.userSrv.update(this.id ,this.userForm.value).subscribe(res=>{
+      this.router.navigate(['users'])
+    })
+  }
+
+}
